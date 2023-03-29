983fb9f843f93dddab2f502accfbc00886c2f211
mjmealey <123016396+mjmealey@users.noreply.github.com>
2023-03-28 22:11:40 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const back = document.getElementById("back")
+
+
+back.addEventListener("click", (e) => {
+    if(e.target.id === "back"){
+        window.location.href = "https://mjmealey.github.io/central-page/"
+    }
+})
