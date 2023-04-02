2f055ed6d201ffc86cbe63134d70babcfe5dee36
mjmealey <123016396+mjmealey@users.noreply.github.com>
2023-04-02 00:03:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const back = document.getElementById("back")
 
 back.addEventListener("click", (e) => {
     if(e.target.id === "back"){
-        window.location.href = "https://mjmealey.github.io/central-page/"
+        window.location.href = "https://mjmealey.github.io/main-page/"
     }
 })
